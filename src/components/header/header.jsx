1a6d606d937967de21b8e2fff8fdd2721049064a
Berezin <pas.1999@mail.ru>
2025-04-02 16:43:55 +0700

    Настроил ссылки в хедере

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,26 @@
+import { useSelector } from 'react-redux';
 import styles from './header.module.scss';
+import { selectUserLogin } from '../../selectors';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
+	const login = useSelector(selectUserLogin);
 	return (
 		<header className={styles.header}>
 			<div className={styles.container}>
 				<div className={styles.links}>
-					<div className={styles.link}>Главная</div>
-					<div className={styles.link}>Проекты</div>
-					<div className={styles.link}>Аналитика</div>
+					<Link to="/" className={styles.link}>
+						Главная
+					</Link>
+					<Link to="/projects" className={styles.link}>
+						Проекты
+					</Link>
+					<Link to="/analitics" className={styles.link}>
+						Аналитика
+					</Link>
 				</div>
 				<div className={styles.user}>
-					<div>user</div>
+					<div>{login}</div>
 					<img
 						src="https://placehold.jp/3d4070/ffffff/40x40.png"
 						alt="avtar"
