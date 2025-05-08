298073f1065b5ca5bac5d0d56fb3c1880e2c775f
Berezin <pas.1999@mail.ru>
2025-05-08 10:53:54 +0700

    сдедал страницу error-page

--- src/pages/error-page/error-page.jsx
+++ src/pages/error-page/error-page.jsx
@@ -1,3 +1,22 @@
+import { Link } from 'react-router-dom';
+import { useCheckAuthorizate } from '../../hooks';
+import styles from './error-page.module.scss';
+
 export const ErrorPage = () => {
-	return <div>ERRORORORORO</div>;
+	const isAuth = useCheckAuthorizate();
+
+	return (
+		<div className="container">
+			<div className={styles.errorText}>Ошибка 404</div>
+			<div className={styles.text}>
+				Страница не найдена. Проверьте пожалуйста адрес
+				{!isAuth && (
+					<div className={styles.links}>
+						перейти на страницу <Link to="/register">регистрации</Link>,
+						<Link to="/authorization"> авторизации</Link>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
